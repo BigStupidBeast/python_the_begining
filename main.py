f9eccf0286d309a10d7d3cbce08913a23f330e2d
BigStupidBeast <argonmaster.pro@gmail.com>
2022-10-09 10:38:03 +0200

    First browser opening with selenium. TADAAAAA

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+import random
+from selenium import webdriver
+
+my_bro = webdriver.Chrome()
+my_bro.get('https://google.com/')
+
 def add(*some_shift):
+    """
+    Function to explore asterix behavior
+    :param some_shift:
+    :return:
+    """
     print(some_shift)
 
-add(3, 'sd' , 3 , 5)
+
+add(3, 'sd', 3, 5)
+
+print(2**16)
