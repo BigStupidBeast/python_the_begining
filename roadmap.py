96070f6e4e8253eb09050dc5aabfaad2245a03d7
BigStupidBeast <argonmaster.pro@gmail.com>
2022-10-12 23:59:10 +0200

    ROADMAP added

---
+++ roadmap.py
@@ -0,0 +1,32 @@
+'''
+
+''''''
+preconditions
+    config
+        browsers
+        window size - param
+    test data
+        base URL start_point?
+        create
+        preset
+            param
+            pairwise
+            txt json csv
+    create browser
+        checking webdriver 
+        download https://github.com/SergeyPirogov/webdriver_manager/issues
+        
+
+
+tests
+    steps
+    debug-mode stay opened if fall?
+    
+    
+reports
+    screenshots
+        path todo param path 
+    allure
+    telegram
+
+
